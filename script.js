c1fcad0137ef04344d329634a77df86944e927cb
Baltej Randhawa <baltej.randhawa0@gmail.com>
2021-11-22 00:20:57 -0800

    need to fix bug with logic

--- script.js
+++ script.js
@@ -1,51 +1,82 @@
 
 //GETS COMPUTERS MOVE
-function computerPlay(){
+function computerMove(){
     let plays = ["Rock","Paper","Scissors"]
     return plays[Math.floor(Math.random()*3)];
 }
 
 //GET HUMAN MOVEE
 
-function playerSelection(){
+function humanMove(){
     let pMove = window.prompt("Choose Rock, Paper or Scissors");
     pMove.toLowerCase()
-    pMove = pMove.slice(0,1)
+    pMove = pMove.slice(0,0)
     return pMove;
 }
 
 
 //Play 1 Round Of the Game
 function playRound(human , comp){
-
-    if(human == r &&comp == Scissors){
-        return("You Win Rock Beats Scissors ")
+    
+    if (human != "r" || human != "p" || human != "s"){
+        return("Invalid Move!!!")
     }
-    else if(human == p && comp == Rock){
+    if(human == "r" &&comp == "Scissors"){
+        return("You Win Rock Beats Scissors")
+    }
+    if(human == "p" && comp == "Rock"){
         return("You Win Paper Beats Rock")
 
     }
-    else if(human == s && comp == Paper){
+    if(human == "s" && comp == "Paper"){
         return("You Win Scissors Beats Paper")
 
-    }
-    else if (human != r || human != p || human != s){
-        return("Invalid Move!!!")
-    }
-
-    else {
+    }else {
         let c;
-        if(human == r){
+        if(human === "r"){
             let c = "Rock"
         }
-        if(human == p){
+        if(human === "p"){
             let c = "Paper"
         }
-        if(human == S){
+        if(human === "s"){
             let c = "Scissors"
         }
-        return(`You Lose ${comp} beats ${c}`)
+        return(`Computer Wins ${comp} beats ${c}`)
 
     }
 
 }
+
+
+function game(){
+    let round = 0; 
+    let humanScore = 0; 
+    let compScore = 0; 
+    while (round<5) {
+        let human = humanMove(); 
+        console.log(`human ${human}`)
+
+
+        let comp = computerMove(); 
+        console.log(`computer ${comp}`)
+
+
+        let result = playRound(human,comp);
+        let win = result; 
+        result = result.slice(0,3); 
+
+        if(result == "You"){
+            humanScore++; 
+        }else{
+            compScore++; 
+        }
+
+        console.log(win);
+        round++; 
+        
+    }
+console.log(`Final Score \n You: ${humanScore}\n Computer: ${compScore}`)
+
+}
+game()
